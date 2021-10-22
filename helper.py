961ec0b5598ad4d89c09b3c8d0e0859434431094
Yang233666 <42207225+Yang233666@users.noreply.github.com>
2021-10-22 21:54:51 +0800

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,83 @@
+import os, sys, logging, logging.config
+import numpy as np, json
+from nltk.tokenize import word_tokenize
+import pathlib
+
+def checkFile(filename):
+    return pathlib.Path(filename).is_file()
+
+def invertDic(my_map, struct='o2o'):
+    inv_map = {}
+
+    if struct == 'o2o':  # Reversing one-to-one dictionary
+        for k, v in my_map.items():
+            inv_map[v] = k
+
+    elif struct == 'm2o':  # Reversing many-to-one dictionary
+        for k, v in my_map.items():
+            inv_map[v] = inv_map.get(v, [])
+            inv_map[v].append(k)
+
+    elif struct == 'm2ol':  # Reversing many-to-one list dictionary
+        for k, v in my_map.items():
+            for ele in v:
+                inv_map[ele] = inv_map.get(ele, [])
+                inv_map[ele].append(k)
+
+    elif struct == 'm2os':
+        for k, v in my_map.items():
+            for ele in v:
+                inv_map[ele] = inv_map.get(ele, set())
+                inv_map[ele].add(k)
+
+    elif struct == 'ml2o':  # Reversing many_list-to-one dictionary
+        for k, v in my_map.items():
+            for ele in v:
+                inv_map[ele] = inv_map.get(ele, [])
+                inv_map[ele] = k
+    return inv_map
+
+
+# Get embedding of words from gensim word2vec model
+def getEmbeddings(model, phr_list, embed_dims):
+    embed_list = []
+    all_num, oov_num, oov_rate = 0, 0, 0
+    for phr in phr_list:
+        if phr in model.vocab:
+            embed_list.append(model.word_vec(phr))
+            all_num += 1
+        else:
+            vec = np.zeros(embed_dims, np.float32)
+            wrds = word_tokenize(phr)
+            for wrd in wrds:
+                all_num += 1
+                if wrd in model.vocab:
+                    vec += model.word_vec(wrd)
+                else:
+                    vec += np.random.randn(embed_dims)
+                    oov_num += 1
+            if len(wrds) == 0:
+                embed_list.append(vec / 10000)
+            else:
+                embed_list.append(vec / len(wrds))
+    oov_rate = oov_num / all_num
+    print('oov rate:', oov_rate, 'oov num:', oov_num, 'all num:', all_num)
+    return np.array(embed_list)
+
+
+def getLogger(name, log_dir, config_dir):
+    config_dict = json.load(open(config_dir + '/log_config.json'))
+
+    if os.path.isdir(log_dir) == False:  # Make log_dir if doesn't exist
+        os.system('mkdir {}'.format(log_dir))
+
+    config_dict['handlers']['file_handler']['filename'] = log_dir + '/' + name
+    logging.config.dictConfig(config_dict)
+    logger = logging.getLogger(name)
+
+    std_out_format = '%(asctime)s - [%(levelname)s] - %(message)s'
+    consoleHandler = logging.StreamHandler(sys.stdout)
+    consoleHandler.setFormatter(logging.Formatter(std_out_format))
+    logger.addHandler(consoleHandler)
+
+    return logger
